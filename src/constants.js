6017cbb1361368bfe587287e1cfccc9a0c5fd74b
Eric Petersen <ericpetersensa@gmail.com>
2025-08-26 15:27:45 -0700

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+// src/constants.js
+
+// The module’s registry key
+export const MODULE_ID = "lazy-gm-prep";
+
+// All setting keys in one place
+export const SETTINGS = {
+  separatePages: "separatePages",
+  folderName:    "folderName",
+  journalPrefix: "journalPrefix"
+};
+
+// Default values for settings
+export const DEFAULTS = {
+  separatePages: true,
+  folderName:    "Lazy GM Prep",
+  journalPrefix: "Session"
+};
