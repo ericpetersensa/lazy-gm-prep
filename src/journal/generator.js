066c2feffafec447f9665b14c553f5008fb5ffaf
Eric Petersen <ericpetersensa@gmail.com>
2025-09-11 21:51:11 -0700

    Update generator.js

--- src/journal/generator.js
+++ src/journal/generator.js
@@ -61,7 +61,7 @@ export async function createPrepJournal() {
   const folder        = await getOrCreateFolder(folderName);
   const sessionNumber = getNextSessionNumber(folder, journalPrefix);
   const dateStamp     = localISODate();                            // Local (not UTC)
-  const journalName   = `${journalPrefix} ${sessionNumber}: ${dateStamp}`;
+  const journalName   = `${journalPrefix} ${sessionNumber}  (${dateStamp})`;
 
   const pages = separatePages
     ? STEP_DEFS.map((step, idx) => {
