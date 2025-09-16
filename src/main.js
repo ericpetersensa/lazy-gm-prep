6aaca054d2e26da81dddf4f9d52427bed8275720
Eric Petersen <ericpetersensa@gmail.com>
2025-09-16 13:38:28 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,36 @@ import { registerSettings } from "./settings.js";
 import { MODULE_ID } from "./constants.js";
 import { createPrepJournal } from "./journal/generator.js";
 
+// --- Add a visible "Create GM Prep" button to the Journal Directory header (AppV2 & v13+) ---
+
+function ensureInlineHeaderButton(dirEl) {
+  if (!game.user.isGM) return;
+  const header = dirEl.querySelector(".directory-header");
+  if (!header) return;
+  const container =
+    header.querySelector(".action-buttons") ||
+    header.querySelector(".header-actions") ||
+    header.querySelector(".header-controls") ||
+    header;
+  if (container.querySelector('[data-action="lazy-gm-prep-inline"]')) return;
+
+  const btn = document.createElement("button");
+  btn.type = "button";
+  btn.dataset.action = "lazy-gm-prep-inline";
+  btn.classList.add("lazy-gm-prep-btn", "header-control", "create-entry");
+  btn.title = game.i18n.localize("lazy-gm-prep.header.buttonTooltip");
+  btn.innerHTML = `<i class="fa-solid fa-clipboard-list"></i> ${game.i18n.localize("lazy-gm-prep.header.button")}`;
+  btn.addEventListener("click", () => createPrepJournal());
+  container.appendChild(btn);
+}
+
+// Add the button every time the Journal Directory renders
+Hooks.on("renderJournalDirectory", (_app, html) => {
+  ensureInlineHeaderButton(html[0] ?? html);
+});
+
+// --- Standard module setup and triggers ---
+
 Hooks.once("init", () => {
   console.log(`${MODULE_ID} | init`);
   registerSettings();
