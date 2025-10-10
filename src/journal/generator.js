b5a142d4f55f7e4e31be966ecf0926ec395b0882
Eric Petersen <ericpetersensa@gmail.com>
2025-10-10 15:56:07 -0700

    Update generator.js

--- src/journal/generator.js
+++ src/journal/generator.js
@@ -6,7 +6,7 @@ import { createImportantNpcsPage } from './pages/importantNpcs.js';
 import { createDefaultSectionPage } from './pages/defaultSection.js';
 import { createStrongStartPage } from './pages/strongStart.js';
 import { createOutlineScenesPage } from './pages/outlineScene.js';
-import { createFantasticLocationsPage } from './pages/fantasticLocations.js'; // <-- NEW
+import { createFantasticLocationsPage } from './pages/fantasticLocations.js'; // NEW
 import {
   ensureFolder,
   nextSequenceNumber,
@@ -20,14 +20,18 @@ export async function createPrepJournal() {
   const folderName = getSetting('folderName', 'GM Prep');
   const prefix = getSetting('journalPrefix', 'Session');
   const includeDate = !!getSetting('includeDateInName', true);
+
   const charRows = Number(getSetting('initialCharacterRows', 5)) || 5;
-  const npcRows = Number(getSetting('initialNpcRows', 5)) || 5;
+  const npcRows  = Number(getSetting('initialNpcRows', 5)) || 5;
+
   const folderId = await ensureFolder(folderName);
   const seq = nextSequenceNumber(prefix);
   const isFirst = seq === 0;
+
   const entryName = includeDate
     ? `${prefix} ${seq}: ${new Date().toLocaleDateString()}`
     : `${prefix} ${seq}`;
+
   const prev = findPreviousSession(prefix);
 
   if (separate) {
@@ -42,9 +46,11 @@ async function createSeparatePages(entryName, folderId, prevJournal, isFirst, { 
   if (isFirst) {
     pages.push(createGettingStartedPage());
   }
+
   for (const def of PAGE_ORDER) {
     const copyOn = !!getSetting(`copy.${def.key}`, true);
     const prevContent = copyOn ? getPreviousSectionHTML(prevJournal, def) : null;
+
     switch (def.key) {
       case 'secrets-clues':
         pages.push(createSecretsCluesPage(def, prevContent));
@@ -61,13 +67,14 @@ async function createSeparatePages(entryName, folderId, prevJournal, isFirst, { 
       case 'outline-scenes':
         pages.push(createOutlineScenesPage(def, prevContent));
         break;
-      case 'fantastic-locations': // <-- NEW
+      case 'fantastic-locations': // NEW
         pages.push(createFantasticLocationsPage(def, prevContent));
         break;
       default:
         pages.push(createDefaultSectionPage(def, prevContent));
     }
   }
+
   const entry = await JournalEntry.create({ name: entryName, folder: folderId, pages });
   ui.notifications?.info(game.i18n.format('lazy-gm-prep.notifications.created', { name: entry.name }));
   return entry;
@@ -80,9 +87,11 @@ async function createSeparatePages(entryName, folderId, prevJournal, isFirst, { 
 async function createCombinedPage(entryName, folderId, prevJournal, isFirst, { charRows, npcRows }) {
   const combinedPageName = game.i18n.localize("lazy-gm-prep.module.name");
   const sectionHtml = [];
+
   for (const def of PAGE_ORDER) {
     const copyOn = !!getSetting(`copy.${def.key}`, true);
     const prevContent = copyOn ? getPreviousSectionHTML(prevJournal, def) : null;
+
     let pageLike;
     switch (def.key) {
       case 'secrets-clues':
@@ -100,26 +109,31 @@ async function createCombinedPage(entryName, folderId, prevJournal, isFirst, { c
       case 'outline-scenes':
         pageLike = createOutlineScenesPage(def, prevContent);
         break;
-      case 'fantastic-locations': // <-- NEW
+      case 'fantastic-locations': // NEW
         pageLike = createFantasticLocationsPage(def, prevContent);
         break;
       default:
         pageLike = createDefaultSectionPage(def, prevContent);
     }
+
     const title = game.i18n.localize(def.titleKey);
     const content = pageLike?.text?.content ?? "";
     sectionHtml.push(`<h2>${title}</h2>\n${content}`);
   }
+
   const combinedContent = sectionHtml.join("\n<hr>\n");
   const pages = [];
+
   if (isFirst) {
     pages.push(createGettingStartedPage());
   }
+
   pages.push({
     name: combinedPageName,
     type: 'text',
     text: { format: 1, content: combinedContent }
   });
+
   const entry = await JournalEntry.create({ name: entryName, folder: folderId, pages });
   ui.notifications?.info(game.i18n.format('lazy-gm-prep.notifications.created', { name: entry.name }));
   return entry;
